4405a1fd62afa2f497c92426d40b164c443fa5ff
gkarkoski <guilherme.karkoski@gmail.com>
2024-11-24 17:47:51 -0300

    Conexão com o MySQL feita

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
+using MinimalApi.Infraestrutura.Db;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
+
+
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost ("/login", (MinimalApi.DTOs.LoginDTO loginDTO) => {
-    if (loginDTO.Email == "adm@test.com" && loginDTO.Senha =="123456"){
+app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@test.com" && loginDTO.Senha == "123456")
+    {
         return Results.Ok("Login realizado com sucesso");
-    }else{
+    }
+    else
+    {
         return Results.Unauthorized();
     }
 
@@ -14,3 +32,4 @@ app.MapPost ("/login", (MinimalApi.DTOs.LoginDTO loginDTO) => {
 
 app.Run();
 
+//Server=localhost;Database=minimal_api;Uid=root;Pwd=123456;
